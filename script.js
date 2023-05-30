805b2cc1adabfe504f6f57788a9584801772b971
CaiqueLace <134824949+Caicossauro@users.noreply.github.com>
2023-05-30 14:17:39 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function buscarDescricao() {
   var codigo = document.getElementById("codigo").value;
-  var url = "https://raw.githubusercontent.com/Caicossauro/form3/main/asd.json";
+  var url = "https://raw.githubusercontent.com/Caicossauro/form3/asd.json";
 
   axios.get(url)
     .then(function(response) {
