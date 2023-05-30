09d3419c41658e2017971f6954d7204ec9046821
CaiqueLace <134824949+Caicossauro@users.noreply.github.com>
2023-05-30 14:06:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function buscarDescricao() {
+  var codigo = document.getElementById("codigo").value;
+  var url = "https://raw.githubusercontent.com/Caicossauro/form3/main/asd.json";
+
+  axios.get(url)
+    .then(function(response) {
+      var dados = response.data;
+      var descricaoEncontrada = "";
+
+      for (var i = 0; i < dados.length; i++) {
+        if (dados[i]["Código"] == codigo) {
+          descricaoEncontrada = dados[i]["Descrição"];
+          break;
+        }
+      }
+
+      document.getElementById("descricao").textContent = "Descrição: " + descricaoEncontrada;
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
