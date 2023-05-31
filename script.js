4b70be81e6b57435a35f4e332b2de0eadcc4c0b9
Caicossauro <caiquelacerda55@gmail.com>
2023-05-30 21:34:03 -0300

    Att 2.0

--- script.js
+++ script.js
@@ -15,8 +15,37 @@ function buscarDescricao() {
       }
 
       document.getElementById("descricao").textContent = "Descrição: " + descricaoEncontrada;
+      document.getElementById("descricao-hidden").value = descricaoEncontrada; // Atualiza o valor do campo hidden
     })
     .catch(function(error) {
       console.log(error);
+    });
+}
+
+function enviarFormulario() {
+  var nome = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var mensagem = document.getElementById("message").value;
+  var codigo = document.getElementById("codigo").value;
+  var descricao = document.getElementById("descricao-hidden").value; // Obtém o valor do campo hidden
+
+  var data = {
+    name: nome,
+    email: email,
+    message: mensagem,
+    codigo: codigo,
+    descricao: descricao
+  };
+
+  axios.post("https://api.staticforms.xyz/submit", data)
+    .then(function(response) {
+      if (response.data.success) {
+        window.location.href = "obrigado.html";
+      } else {
+        alert("Ocorreu um erro ao enviar o formulário. Por favor, tente novamente.");
+      }
+    })
+    .catch(function(error) {
+      alert("Ocorreu um erro ao enviar o formulário. Por favor, tente novamente.");
     });
 }
